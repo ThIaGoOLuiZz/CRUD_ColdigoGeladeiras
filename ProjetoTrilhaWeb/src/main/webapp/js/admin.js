10cb9f7ddbfe35bdc38bfb91b38e907b00064678
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2024-03-25 19:11:39 -0300

    Adicionado tela de admin

---
+++ ProjetoTrilhaWeb/src/main/webapp/js/admin.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    $("header").load("http://localhost:8085/ProjetoTrilhaWeb/pages/admin/general/header.html");
+    $("footer").load("http://localhost:8085/ProjetoTrilhaWeb/pages/admin/general/footer.html");
+});
