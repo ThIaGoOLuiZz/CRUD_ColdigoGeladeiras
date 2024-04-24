c6418cf8b79bd48462b0a51b72087aaf80d6728a
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2024-04-23 21:33:42 -0300

    Adicionado tela de edição

--- ProjetoTrilhaWeb/src/main/webapp/js/product.js
+++ ProjetoTrilhaWeb/src/main/webapp/js/product.js
@@ -117,7 +117,7 @@ $(document).ready(function() {
 						"<td>" + listaDeProdutos[i].capacidade + "</td>" +
 						"<td>R$" + COLDIGO.formatarDinheiro(listaDeProdutos[i].valor) + "</td>" +
 						"<td>" +
-							"<a><img src='../../imgs/edit.png' alt='Editar registro'></a>" +
+							"<a onclick=\"COLDIGO.produto.exibirEdicao('" +listaDeProdutos[i].id+ "')\"><img src='../../imgs/edit.png' alt='Editar registro'></a>" +
 							"<a onclick=\"COLDIGO.produto.excluir('" + listaDeProdutos[i].id + "')\"><img src='../../imgs/delete.png' alt='Excluir registro'></a>" +
 						"</td>" +
 						"</tr>"
@@ -148,4 +148,52 @@ $(document).ready(function() {
 			}
 		});
 	};
+	
+	COLDIGO.produto.exibirEdicao = function(id){
+		$.ajax({
+			type:"GET",
+			url: COLDIGO.PATH + "produto/buscarPorId",
+			data: "id=" + id,
+			success: function(produto){
+				
+				document.frmEditaProduto.idProduto.value = produto.id;
+				document.frmEditaProduto.modelo.value = produto.modelo;
+				document.frmEditaProduto.capacidade.value = produto.capacidade;
+				document.frmEditaProduto.valor.value = produto.valor;
+				
+				var selCategoria = document.getElementById('selCategoriaEdicao')
+				for(var i=0; i < selCategoria.length; i++){
+					if (selCategoria.options[i].value == produto.categoria){
+						selCategoria.options[i].setAttribute("selected", "selected");
+					}else{
+						selCategoria.options[i].removeAttribute("selected");
+					}
+				}
+				
+				var modalEditaProduto = {
+					title: "Editar Produto",
+					height: 400,
+					width: 550,
+					modal: true,
+					buttons:{
+						"Salvar": function(){
+							
+						},
+						"Cancelar": function(){
+							$(this).dialog("close");
+						}
+					},
+					close: function(){
+						//CASO FECHE A CAIXA DE EDIÇÃO NADA ACONTECE
+					}
+				}
+				
+				$("#modalEditaProduto").dialog(modalEditaProduto);	
+				
+			},
+			error: function(info){
+				COLDIGO.exibirAviso("Erro ao buscar produto para edição: " + info.status + " - " + info.statusText);
+			}
+		})
+	}
 });
