cacbf6357a64c7548bee0cb26d9fac747b183cc0
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2024-04-25 21:19:50 -0300

    Finalizado CRUD produtos

--- ProjetoTrilhaWeb/src/main/webapp/js/admin.js
+++ ProjetoTrilhaWeb/src/main/webapp/js/admin.js
@@ -16,6 +16,8 @@ $(document).ready(function() {
     //FUNÇÃO PARA CARREGAMENTO DE PÁGINAS DE CONTEÚDO, QUE RECEBE COMO PARAMETRO O NOME DA PASTA COM A PÁGINA A SER CARREGADA
     
     COLDIGO.carregaPagina = function(pagename){
+		if($(".ui-dialog"))
+			$(".ui-dialog").remove();		
 		//LIMPA A TAG SECTION, EXCLUINDO TODO O CONTEUDO DE DENTRO DELA
 		$("section").empty();
 		//CARREGA A PÁGINA SOLICITADA DENTRO 	DA TAG SECTION
